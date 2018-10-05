63bf5a9d712b6997e93e72d628139ad28694a505
Adrian Schoenig <adrian.schoenig@gmail.com>
2018-10-05 19:32:48 +0200

    Add basic generator

---
+++ lib/jekyll/maparoni/generator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Jekyll
+	module Maparoni
+		class Generator < Jekyll::Generator
+			safe true
+	    priority :lowest
+
+			def generate(site)
+				@site = site
+				site.collections.each do |name, collection|
+					Jekyll.logger.info "Jekyll Maparoni:", "Generating geojson for #{name}"
+					@site.pages << make_geojson(name, collection.metadata["title"])
+				end
+			end
+
+			private
+
+			def make_geojson(collection, title)
+				source_path = File.expand_path "maparoni.geojson", __dir__
+				template = File.read(source_path)
+
+				Jekyll::PageWithoutAFile.new(@site, __dir__, "", collection + ".geojson").tap do |file|
+					file.content = template
+					file.data["layout"] = nil
+					file.data["collection"] = collection
+					file.data["label"] = title # Don't use ["title"] or it might show up in the navigation!
+					file
+				end
+			end
+		end
+	end
+end
